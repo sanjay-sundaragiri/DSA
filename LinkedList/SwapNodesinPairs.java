b52e223bc1ec6dd86866d70a0efb234f0fc516c2
sanjay-sundaragiri <sanjaysundaragiri@gmail.com>
2022-12-05 21:51:43 +0530

    recursion

---
+++ LinkedList/SwapNodesinPairs.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        
+       
+        ListNode newNode = head.next;
+        head.next = swapPairs(head.next.next);   //TC: O(N). SC:O(1)
+        
+        newNode.next=head;
+        
+        
+        return newNode;
+        
+        
+    }
+    
+   
+}
